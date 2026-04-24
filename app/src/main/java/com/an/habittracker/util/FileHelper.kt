670ce27112be60a85abcd7db4247a901bd267b70
Y14L <yosuaimmanuelariyasalianoto@gmail.com>
2026-04-24 22:13:25 +0700

    Remove "NoActionBar"

---
+++ app/src/main/java/com/an/habittracker/util/FileHelper.kt
@@ -0,0 +1,45 @@
+package com.an.habittracker.util
+
+import android.content.Context
+import java.io.File
+import java.io.IOException
+import java.io.FileOutputStream
+
+class FileHelper(val context: Context) {
+    val folderName = "myfolder"
+    val fileName = "mydata.txt"
+
+    private fun getFile(): File {
+        val dir = File(context.filesDir, folderName)
+        if (!dir.exists()) {
+            dir.mkdirs()
+        }
+        return File(dir, fileName)
+    }
+
+    fun writeToFile(data: String) {
+        try {
+            val file = getFile()
+            FileOutputStream(file, false).use { output ->
+                output.write(data.toByteArray())
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    fun readFromFile(): String {
+        return try {
+            val file = getFile()
+            file.bufferedReader().useLines { lines ->
+                lines.joinToString("\n")
+            }
+        } catch (e: IOException) {
+            e.printStackTrace().toString()
+        }
+    }
+
+    fun getFilePath(): String {
+        return getFile().absolutePath
+    }
+}
